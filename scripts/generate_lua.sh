cea33bdcd37b7dc4f2d12d9734adbbea9de1f296
Steve Vermeulen <sfvermeulen@gmail.com>
2024-08-19 14:44:15 +0100

    Added helper scripts to set up local luarocks install

---
+++ scripts/generate_lua.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+cd `dirname $BASH_SOURCE`/..
+rm -rf ./gen
+mkdir ./gen
+mkdir ./gen/lusc
+mkdir ./gen/lusc/internal
+mkdir ./gen/lusc/tests
+tl gen src/lusc/init.tl -o gen/lusc/init.lua
+tl gen src/lusc/internal/util.tl -o gen/lusc/internal/util.lua
+cp src/lusc/internal/queue.lua gen/lusc/internal/queue.lua
+tl gen src/lusc/tests/async_helper.tl -o gen/lusc/tests/async_helper.lua
+tl gen src/lusc/tests/lusc_spec.tl -o gen/lusc/tests/lusc_spec.lua
+tl gen src/lusc/tests/setup.tl -o gen/lusc/tests/setup.lua
+tl gen src/lusc/luv_async.tl -o gen/lusc/luv_async.lua
+tl gen src/lusc/tests/luv_async_spec.tl -o gen/lusc/tests/luv_async_spec.lua
